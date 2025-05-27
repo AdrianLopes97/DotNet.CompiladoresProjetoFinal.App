4cb14369cfc7a3844cfb0fc7c15421f518299b73
AdrianLopes97 <adrianlopes97@rede.ulbra.br>
2025-05-27 15:23:59 -0300

    Desenvolvimento da nova gramatica - trabalho final etapa 1

--- DotNet.CompiladoresProjetoFinal.App/Program.cs
+++ DotNet.CompiladoresProjetoFinal.App/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Antlr4.Runtime;
+using DotNet.CompiladoresProjetoFinal.App;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = Path.Combine("ANTLR", "VariableDeclarationTestCases.txt");
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine($"Arquivo não encontrado: {filePath}");
+            return;
+        }
+
+        var lines = File.ReadAllLines(filePath);
+        int lineNumber = 0;
+
+        foreach (var rawLine in lines)
+        {
+            var input = rawLine.Trim();
+
+            // Ignora linhas em branco e comentários
+            if (string.IsNullOrWhiteSpace(input) || input.StartsWith("#"))
+                continue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Linha {lineNumber}: {input}");
+
+            AntlrInputStream inputStream = new AntlrInputStream(input);
+            VariableDeclarationLexer lexer = new VariableDeclarationLexer(inputStream);
+            CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+            VariableDeclarationParser parser = new VariableDeclarationParser(tokenStream);
+
+            parser.RemoveErrorListeners();
+            parser.AddErrorListener(ThrowingErrorListener.Instance);
+
+            try
+            {
+                var tree = parser.declaration();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Válida");
+                Console.WriteLine(tree.ToStringTree(parser));
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Inválida: " + e.Message);
+            }
+            lineNumber++;
+        }
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+    }
+}
